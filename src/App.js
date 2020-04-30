c5afef98c0397c43cd8d67da448791cd8c0b3693
axix.szabist@gmail.com <IA4188@adda.gov.ae>
2020-04-30 22:56:56 +0400

    adding language

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Suspense, Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { Header } from "./header/header";
@@ -6,11 +6,69 @@ import Footer from "./footer/footer";
 import Bodycards from "./bodycards";
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './i18n';
+import { withTranslation } from 'react-i18next';
+import i18next from 'i18next';
+class App extends Component {
+  handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
 
+  render() {
+    const { t } = this.props;
+
+    let somthingNeedsTobe = "";
+    let tagetedValue = 1;
+    //when you want it to be rtl 
+    if (tagetedValue === 1) {
+      somthingNeedsTobe = "rtl"
+    } else {
+      somthingNeedsTobe = "ltr"
+    }
+
+    return (
+      <div className="App">
+        <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+          <button onClick={() => this.handleClick('en')} >
+            English
+        </button>
+          <button onClick={() => this.handleClick('ko')} >
+            Korean
+        </button>
+          <button onClick={() => this.handleClick('chi')} >
+            Chinese
+          </button>
+        </nav>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            <p>{t('Thanks.1')}</p>  <p>{t('Why.1')}</p>
+            {somthingNeedsTobe === "rtl" ?
+              t('Position1') :
+              t('Position2')
+            }
+            {somthingNeedsTobe === "rtl" ?
+              //Load External Css A
+              <></>
+              : 
+              <></>
+              //Load External Css B
+            }
+
+          </div>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default withTranslation()(App);
+/*
 function App() {
   return (
     <div className="App">
       <div className="row"><Header /></div>
+      <h1>{useTranslation('Welcome to React')}</h1>
       <br></br>
       <div className="row">
         <div className="col-sm-4"><Bodycards /></div>
@@ -29,3 +87,4 @@ function App() {
 }
 
 export default App;
+*/
