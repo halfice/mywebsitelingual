7762410441122aa9579c152caed9fecd2831aa9c
Abdul Aziz Farooqi <abdulazizfarooqi@Abduls-MacBook-Pro.local>
2020-05-02 00:05:36 +0400

    Adding more item

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Suspense, Component } from 'react'
+import React, { Suspense, Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 import { Header } from "./header/header";
@@ -6,8 +6,8 @@ import Footer from "./footer/footer";
 import Bodycards from "./bodycards";
 import Loginsecion from './loginsecion'
 
-
-
+import Looking from './lookingforroom';
+import RoomOwner from './roomowner';
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './i18n';
@@ -18,7 +18,32 @@ import i18next from 'i18next';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      needwizard: 0,
+      ownerwizard:0,
+    }
+   
+  }
+
   
+
+
+  handler = (val) => {
+  
+    this.setState({
+      needwizard: val
+    })
+  }
+
+  handlertwo = (val) => {
+  
+    this.setState({
+      ownerwizard: val
+    })
+  }
 
   render() {
     const { t } = this.props;
@@ -36,37 +61,57 @@ class App extends Component {
       <div className="App">
 
 
-           <div className="row"><Header /></div>
+        <div className="row"><Header /></div>
 
 
 
 
-           <br></br>
-           <div className="row"><Loginsecion /></div>
-
-
-           <div className="row">
-        <div className="col-sm-4"><Bodycards /></div>
-        <div className="col-sm-4"><Bodycards /></div>
-        <div className="col-sm-4"><Bodycards /></div>
-      </div>
-          <div>
-            <p>{t('Thanks.1')}</p>  <p>{t('Why.1')}</p>
-            {somthingNeedsTobe === "rtl" ?
-              t('Position1') :
-              t('Position2')
-            }
-            {somthingNeedsTobe === "rtl" ?
-              //Load External Css A
-              <></>
-              : 
-              <></>
-              //Load External Css B
-            }
-
+        <br></br>
+        {this.state.needwizard == 0 &&  
+        <div>
+           <div className="row"><Loginsecion handler = {this.handler} /></div>
           </div>
-          <div className="row"><Footer /></div>
-        
+        }
+       
+       {this.state.needwizard == 1 &&  
+        <div>
+           <div className="row"><Looking  /></div>
+          </div>
+        }
+
+        <div className="row">
+        {
+        this.state.needwizard ==0 && 
+        <div>
+          <div className="col-sm-12"><Bodycards   /></div>
+          </div>
+          }
+
+{
+        this.state.needwizard ==3 && 
+        <div>
+          <div className="col-sm-12"><RoomOwner   /></div>
+          </div>
+          }
+
+        </div>
+        <div>
+          <p>{t('Thanks.1')}</p>  <p>{t('Why.1')}</p>
+          {somthingNeedsTobe === "rtl" ?
+            t('Position1') :
+            t('Position2')
+          }
+          {somthingNeedsTobe === "rtl" ?
+            //Load External Css A
+            <></>
+            :
+            <></>
+            //Load External Css B
+          }
+
+        </div>
+        <div className="row"><Footer /></div>
+
       </div>
     )
   }
@@ -77,8 +122,8 @@ export default withTranslation()(App);
 function App() {
   return (
     <div className="App">
-    
-     
+
+
       <div className="row">
         <div className="col-sm-4"><Bodycards /></div>
         <div className="col-sm-4"><Bodycards /></div>
---
+++ src/lookingforroom.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTumblr, faTwitter } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCoffee,
+  faCog,
+  faSpinner,
+  faQuoteLeft,
+  faSquare,
+  faCheckSquare
+} from '@fortawesome/free-solid-svg-icons'
+
+import './i18n';
+import { withTranslation } from 'react-i18next';
+import i18next from 'i18next';
+export class lookingoforroom extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 2,
+    }
+  }
+  shoonChangewsp() {
+    this.setState({
+      value: 4
+    });
+  }
+
+  render() {
+    return (
+      <div>
+       <h1>Wizard for looking for flat mate</h1>
+        </div>
+
+      
+      
+    );
+  }
+
+}
+
+export default withTranslation()(lookingoforroom);
